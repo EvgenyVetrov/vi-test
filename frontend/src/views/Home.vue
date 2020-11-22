7e0a5e540c97f2b8f987da6a6c232ab0fb5d8ba5
evetrov <post.vetrov@gmail.com>
2020-11-22 14:49:16 +0300

    vue загрузка скелета

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container style="max-width: 1024px">
+
+    <v-row class="text-center">
+      <v-col>
+        <v-img
+                :src="require('../assets/logo.svg')"
+                class="my-3"
+                contain
+                height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Vuetify
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+          <br>please join our online
+          <a
+                  href="https://community.vuetifyjs.com"
+                  target="_blank"
+          >Discord Community</a>
+        </p>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" md="4" sm="6"><h3>каталог</h3></v-col>
+      <v-col cols="12" md="4" sm="6"><h3>корзина</h3></v-col>
+      <v-col cols="12" md="4" sm="6"><h3>заказы</h3></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+//import StoreCarcass from "../components/StoreCarcass";
+
+export default {
+  name: 'Home',
+  components: {
+    //  StoreCarcass,
+    //HelloWorld
+  }
+}
+</script>
